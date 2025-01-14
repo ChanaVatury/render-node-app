567e163fec5e235088525d6871b9fcc648cd5dfd
ChanaVatury <cv3285191@gmail.com>
2025-01-14 22:19:51 +0200

    first commit

---
+++ z.js
@@ -0,0 +1,77 @@
+[
+    {
+      "cursor": "466mVsKdDcU1NTl1MGptczczZG1udTIw",
+      "service": {
+        "autoDeploy": "yes",
+        "branch": "main",
+        "createdAt": "2025-01-14T17:44:53.851159Z",
+        "dashboardUrl": "https://dashboard.render.com/static/srv-cu3a559u0jms73dmnu20",
+        "id": "srv-cu3a559u0jms73dmnu20",
+        "name": "ToDoListReact",
+        "notifyOnFail": "default",
+        "ownerId": "tea-cu1vurggph6c73els2bg",
+        "repo": "https://github.com/ChanaVatury/ToDoListReact",
+        "rootDir": "",
+        "serviceDetails": {
+          "buildCommand": "npm run build",
+          "buildPlan": "starter",
+          "previews": {
+            "generation": "off"
+          },
+          "publishPath": "build",
+          "pullRequestPreviewsEnabled": "no",
+          "url": "https://todolistreact-3kna.onrender.com"
+        },
+        "slug": "todolistreact-3kna",
+        "suspended": "not_suspended",
+        "suspenders": [],
+        "type": "static_site",
+        "updatedAt": "2025-01-14T19:51:50.059428Z"
+      }
+    },
+    {
+      "cursor": "XH2Dg7P6IOVyNWpxZjB1czczZTlvNjVn",
+      "service": {
+        "autoDeploy": "yes",
+        "branch": "main",
+        "createdAt": "2025-01-14T17:23:34.901333Z",
+        "dashboardUrl": "https://dashboard.render.com/web/srv-cu39r5jqf0us73e9o65g",
+        "id": "srv-cu39r5jqf0us73e9o65g",
+        "name": "ToDoApi",
+        "notifyOnFail": "default",
+        "ownerId": "tea-cu1vurggph6c73els2bg",
+        "repo": "https://github.com/ChanaVatury/ToDoApi",
+        "rootDir": "",
+        "serviceDetails": {
+          "buildPlan": "starter",
+          "env": "docker",
+          "envSpecificDetails": {
+            "dockerCommand": "",
+            "dockerContext": ".",
+            "dockerfilePath": "Dockerfile"
+          },
+          "healthCheckPath": "",
+          "maintenanceMode": {
+            "enabled": false,
+            "uri": ""
+          },
+          "numInstances": 1,
+          "openPorts": null,
+          "plan": "free",
+          "previews": {
+            "generation": "off"
+          },
+          "pullRequestPreviewsEnabled": "no",
+          "region": "singapore",
+          "runtime": "docker",
+          "sshAddress": "srv-cu39r5jqf0us73e9o65g@ssh.singapore.render.com",
+          "url": "https://todoapi-qiur.onrender.com"
+        },
+        "slug": "todoapi-qiur",
+        "suspended": "not_suspended",
+        "suspenders": [],
+        "type": "web_service",
+        "updatedAt": "2025-01-14T19:21:28.9251Z"
+      }
+    }
+  ]
